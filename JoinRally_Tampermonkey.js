ecd2996db834fb1c04911de560a11b8b3fc10101
yogahariman <yogahariman.course@gmail.com>
2025-05-28 18:00:57 +0700

    ok

--- JoinRally_Tampermonkey.js
+++ JoinRally_Tampermonkey.js
@@ -11,21 +11,16 @@
 (function() {
     'use strict';
 
-    // 💡 Pastikan variabel bisa diakses oleh script eksternal
-    //window.tokenTelegram = '1936285843:AAFgubrFQcbz0B7zN8hUKS2oNLPS-Nyyxyw'; // ← ganti token
-    //window.delayCheckListRally = 90000;
-    //window.shouldOpenChest = true;
-    //window.shouldOpenFreeChest = true;
-    //window.shouldSearchTower = true;
 
-
+    // Setting Troops
+    // 50100306=T6, 50100305=T5, 50100304=T4
     window.troopCodes = [50100306, 50100305, 50100304];
-    window.troopAmounts = [0, 250000, 0];
+    window.troopAmounts = [0, 100000, 0];
     window.allowedMonsters = {
-        "20200201": { name: "DeathKar", minLevel: 4 },
-        "20200202": { name: "Green Dragon", minLevel: 99 },
+        "20200201": { name: "DeathKar", minLevel: 1 },
+        "20200202": { name: "Green Dragon", minLevel: 1 },
         "20200203": { name: "Red Dragon", minLevel: 1 },
-        "20200204": { name: "Yellow Dragon", minLevel: 99 },
+        "20200204": { name: "Yellow Dragon", minLevel: 1 },
         "20200206": { name: "Panta", minLevel: 1 },
         "20200207": { name: "Gargantua", minLevel: 1 },
         "20700505": { name: "Magdar", minLevel: 1 },
