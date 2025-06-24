1056ff6d191e3a4181bd40a58a92db2c3d8e391d
yogahariman <yogahariman.course@gmail.com>
2025-06-24 12:35:47 +0700

    lebar dan tinggi firefox dalam bentuk persentase

--- lok.sh
+++ lok.sh
@@ -3,9 +3,20 @@
 # ./lok.sh user1 user2 user3
 # sudo apt-get install wmctrl xdotool
 
-# Ukuran jendela Firefox
-window_width=750
-window_height=1050
+
+# Konfigurasi dalam persentase
+window_width_percent=40    # % dari lebar layar
+window_height_percent=100  # % dari tinggi layar
+
+# Ambil resolusi layar
+read screen_width screen_height < <(xdotool getdisplaygeometry)
+
+# Hitung ukuran jendela berdasarkan persentase
+window_width=$((screen_width * window_width_percent / 100))
+window_height=$((screen_height * window_height_percent / 100))
+
+echo "Resolusi layar: ${screen_width}x${screen_height}"
+echo "Ukuran jendela Firefox: ${window_width}x${window_height} (${window_width_percent}% x ${window_height_percent}%)"
 
 # Fungsi untuk membuka Firefox dan memindahkan ke kanan layar
 launch_firefox() {
@@ -16,14 +27,13 @@ launch_firefox() {
     firefox -P "$profile" --new-window "https://www.google.com/" -width "$window_width" -height "$window_height" --no-remote &
     
     local pid=$!
-    sleep 10  # Tunggu Firefox terbuka
+    sleep 5  # Tunggu Firefox terbuka
 
     # Ambil ID jendela berdasarkan PID
     win_id=$(xdotool search --sync --all --pid $pid --onlyvisible | head -n 1)
     
     if [ -n "$win_id" ]; then
         echo "Memindahkan jendela $win_id ke sisi kanan layar"
-        screen_width=$(xdotool getdisplaygeometry | awk '{print $1}')
         target_x=$((screen_width - window_width))
         xdotool windowmove "$win_id" "$target_x" 0
     else
@@ -35,7 +45,6 @@ launch_firefox() {
 for profile in "$@"; do
     launch_firefox "$profile"
 done
-
 
 
 # #!/bin/bash
