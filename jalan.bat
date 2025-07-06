ce1cec825d1199faf9c4b0d92c5a5a0db1391946
yogahariman <yogahariman.course@gmail.com>
2025-07-06 17:28:49 +0700

    -

--- jalan.bat
+++ jalan.bat
@@ -1,4 +1,15 @@
 @echo off
+title Menjalankan jalan.py dengan Virtual Env
+
+echo [INFO] Mengaktifkan virtual environment...
 call C:\Users\user\my_env\Scripts\activate.bat
+
+echo [INFO] Menjalankan script jalan.py...
 python C:\Users\user\jalan.py
+
+echo [INFO] Selesai. Menonaktifkan environment...
+call deactivate
+
+echo.
+echo [SELESAI] Tekan tombol apa saja untuk keluar.
 pause
