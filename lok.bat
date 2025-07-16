4706ef1cc002655e4c939659754ee9aed0fe945e
yogahariman <yogahariman.course@gmail.com>
2025-07-16 21:29:10 +0700

    -

--- lok.bat
+++ lok.bat
@@ -1,19 +1,30 @@
-:: lok.bat user1 user2 user3
 @echo off
 setlocal enabledelayedexpansion
 
-:: Fungsi untuk menjalankan Firefox dengan profil dan URL
-:launch_firefox
-set "profile=%~1"
-echo Menjalankan Firefox dengan profil: %profile%
-start "" "C:\Program Files\Mozilla Firefox\firefox.exe" -P "%profile%" --new-window "https://www.google.com/" -width 700 -height 600 --no-remote
-timeout /t 5 >nul
-goto :eof
+:: Jumlah profil
+set "jumlah_profil=7"
 
-:: Loop untuk semua argumen
-for %%p in (%*) do (
-    call :launch_firefox %%p
+:: Path Firefox
+set "firefox_path=C:\Program Files\Mozilla Firefox\firefox.exe"
+
+:: Ukuran jendela manual (px)
+set "window_width=900"
+set "window_height=980"
+
+:: Cek Firefox
+if not exist "%firefox_path%" (
+    echo Firefox tidak ditemukan di: %firefox_path%
+    pause
+    exit /b
 )
 
-endlocal
+:: Loop jalankan profil
+for /L %%i in (1,1,%jumlah_profil%) do (
+    set "profile=user%%i"
+    echo Menjalankan Firefox dengan profil: !profile!
+    start "" "%firefox_path%" -P "!profile!" --no-remote -width %window_width% -height %window_height%
+    timeout /t 3 /nobreak >nul
+)
+
+echo Selesai menjalankan semua profil.
 pause
