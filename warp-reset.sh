7895a8ea5b6790bfede056de6689a08dd62e0054
yogahariman <yogahariman.course@gmail.com>
2025-08-19 17:04:03 +0700

    warp-reset

--- warp-reset.sh
+++ warp-reset.sh
@@ -1,19 +1,35 @@
-# restIP.sh
+#!/bin/bash
+# === Resetting WARP and Checking IP ===
+
+# Disconnect
 warp-cli disconnect
 sleep 3
+
+# Delete registration
 warp-cli registration delete
 sleep 3
+
+# New registration
 warp-cli registration new
-#warp-cli registration license vn1s79T2-L0nS217q-zu5a049W
-#service systemd-resolved restart
+# warp-cli set-license vn1s79T2-L0nS217q-zu5a049W   # kalau mau pakai license, hapus tanda #
+
 sleep 3
+
+# Connect again
 warp-cli connect
-#warp-cli mode warp+doh
-#warp-cli dns families off
+# warp-cli mode warp+doh
+# warp-cli dns families off
+
 sleep 5
-#curl https://www.cloudflare.com/cdn-cgi/trace/
+
+# Check IP
 curl ifconfig.co/
 sleep 5
 curl ifconfig.co/city
-now=$(date +"%T")
+
+# Show current time
+now=$(date +"%H:%M:%S")
 echo "Current time : $now"
+
+echo
+echo "=== Done! ==="
