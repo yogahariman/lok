4d153bb67002fdf287c6478467ead65a72f83d2d
Yoga Hariman <yogahariman.course@gmail.com>
2026-01-06 11:21:23 +0700

    -

--- JoinRallyAdvance.js
+++ JoinRallyAdvance.js
@@ -1329,7 +1329,7 @@ async function claimMainQuest() {
     }
 }
 
-async function claimMainQuestRepeat() {
+async function claimMainQuestAll() {
     console.log("🚀 Mulai klaim main quest");
 
     while (true) {
